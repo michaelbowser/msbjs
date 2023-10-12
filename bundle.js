a036ac40e1e08b1686a9f41a1c33482036fa5236
michaelbowser <brightgeist@yahoo.com>
2023-10-12 15:27:02 -0500

    LE 9.13 changed require to mbpalindrome

--- bundle.js
+++ bundle.js
@@ -2,21 +2,22 @@
 let Phrase = require("mbpalindrome");
 
 function palindromeTester() {
-    let string = prompt("Please enter string for palindrome testing:");
-    let phrase = new Phrase(string);
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome. `)
-}
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-let button = document.querySelector("#palindromeTester");
-button.addEventListener("click", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
     palindromeTester();
-});
+  });
 });
 },{"mbpalindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let Phrase = require("<username>-palindrome");
+let Phrase = require("mbpalindrome");
 
 function palindromeTester() {
   let string = prompt("Please enter a string for palindrome testing:");
