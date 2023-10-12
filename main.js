b7a7168cd28be2d908a218237f56af7ad3ec3f4c
michaelbowser <brightgeist@yahoo.com>
2023-10-12 15:03:34 -0500

    LE 9.9

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let Phrase = require("mbpalindrome");
 
-let string = prompt("Please enter string for palindrome testing:");
-let phrase = new phrase(string);
+function palindromeTester() {
+    let string = prompt("Please enter string for palindrome testing:");
+    let phrase = new Phrase(string);
 
 if (phrase.palindrome()) {
     alert(`"${phrase.content}" is a palindrome!`);
 } else {
     alert(`"${phrase.content}" is not a palindrome. `)
 }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+let button = document.querySelector("#palindromeTester");
+button.addEventListener("click", function() {
+    palindromeTester();
+});
+});
